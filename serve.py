bc157a99d2a28b60e6367549be3ca061485e8c64
Toba <Toba.akanbi@ollasystems.com>
2026-07-08 13:36:47 +0100

    rmvd some files

--- serve.py
+++
@@ -1,5 +0,0 @@
-import http.server, socketserver, os
-os.chdir('/Users/toba/Desktop/ollacloud')
-handler = http.server.SimpleHTTPRequestHandler
-with socketserver.TCPServer(('', 8099), handler) as httpd:
-    httpd.serve_forever()
